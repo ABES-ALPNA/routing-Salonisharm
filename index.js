6c4fabc6128288f405082924d1ad1a1b61c96153
saloni sharma <143931745+Salonisharm@users.noreply.github.com>
2025-08-27 10:05:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+//const data= require("fs")
+//data.writeFileSync("abs.txt","hello")
+
+//const readData = data.readFileSync("abs.txt","utf-8")
+//console.log(readData)
+//console.log(data.platform())
+//function sum (a,b){
+  //return a+b
+//}
+//function sub(a,b){
+ // return a-b
+//}
+//module.exports=
+//{sum,sub}
+const data =require("chalk")
+console.log(data.green("hello saloni"))
+console.log(data.bgBlue("blue"))
